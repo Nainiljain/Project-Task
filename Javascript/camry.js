c50d1793ab1541f37207f05c13e8610b77218f9c
Nainil <nainiljain1@gmail.com>
2024-10-26 20:25:54 -0400

    Honda accord page is compeleted

--- Javascript/camry.js
+++ Javascript/camry.js
@@ -32,29 +32,3 @@ tabButtons.forEach(button => {
         }
     });
 });
-function showMenu() {
-    document.getElementById("dropdown-content").style.display = "block";
-}
-
-function hideMenu() {
-    document.getElementById("dropdown-content").style.display = "none";
-}
-// script.js
-function changeReadMore() {
-    const mycontent =
-        document.getElementById('mybox1id');
-    const mybutton =
-        document.getElementById('mybuttonid');
-    const span1 = document.getElementById("span1")
-
-    if (mycontent.style.display === 'none'
-        || mycontent.style.display === '') {
-        mycontent.style.display = 'inline';
-        span1.style.display = "none";
-        mybutton.textContent = 'Read Less';
-    } else {
-        mycontent.style.display = 'none';
-        mybutton.textContent = 'Read More';
-        span1.style.display = "inline";
-    }
-}
