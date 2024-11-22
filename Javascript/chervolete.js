da9cd63dd823208227b64fed6320ef8c208637cc
Nainil Jain <nainiljain1@gmail.com>
2024-11-22 15:46:45 -0500

    Home page and the news page working

--- Javascript/chervolete.js
+++ Javascript/chervolete.js
@@ -119,3 +119,19 @@ function displayReview(reviewData) {
 
     document.getElementById("reviews").appendChild(reviewItem);
 }
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.querySelectorAll('.slides img');
+  if (n >= slides.length) { slideIndex = 0 }
+  if (n < 0) { slideIndex = slides.length - 1 }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex].style.display = 'block';
+}
