3d02ad972e4619b1bf876c85c83dc89bcf82f006
Nainil Jain <nainiljain1@gmail.com>
2024-12-12 18:53:03 -0500

    working home page

--- Javascript/register.js
+++ Javascript/register.js
@@ -1,27 +1,20 @@
-const register = () => {
+const register = () => 
+{
     var Firstname = document.getElementById("firstname").value;
     var Lastname = document.getElementById("lastname").value;
     var email = document.getElementById("email").value;    
-<<<<<<< HEAD
     var password = document.getElementById("password").value;
-=======
-    var password = document.getElementById("password").value
->>>>>>> c7ddc5a983b72a6104315629f08a61d261c15f12
 
     var user = {
-        Firstname: Firstname,
-        Lastname: Lastname,
-        email: email,
-        password: password
+        Firstname= Firstname,
+        Lastname= Lastname,
+        email= email,
+        password= password
     };
     var json = JSON.stringify(user);
-    localStorage.setItem(email, json);
-<<<<<<< HEAD
-    console log("User added:", user);
-=======
+    localStorage.setItem("email", json);
     console.log("User added:", user);
->>>>>>> c7ddc5a983b72a6104315629f08a61d261c15f12
-    alert("Registration successful!");
+    alert("Registration successful!")
 
     window.location.href="signin.html";
 };
